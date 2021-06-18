0517121d66cfe839b0e344f9cba92750ecb00e6c
Evangelos V <evangv12@gmail.com>
2021-06-18 20:14:37 +0300

    Adding semester exercises, initial exercise added

---
+++ Part1/2021-03-02_int_addition.c
@@ -0,0 +1,22 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+//remove 0 from main to play
+
+int main0(void) {
+
+	int a, b;
+
+	printf("Enter a: ");
+	scanf("%d", &a);
+
+	printf("Enter b: ");
+	scanf("%d", &b);
+
+
+	printf("a+b=%d", a + b);
+
+
+	return 0;
+}
+
