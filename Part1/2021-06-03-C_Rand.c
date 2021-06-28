a15fafafbe3c8494475e6e800c846bb7dba0a3c4
Evangelos V <evangv12@gmail.com>
2021-06-28 23:19:53 +0300

    restructuring

---
+++ Part1/2021-06-03-C_Rand.c
@@ -0,0 +1,21 @@
+#pragma warning(disable : 4996)
+#include <stdio.h>
+
+#include <time.h>
+#include <stdlib.h>
+
+//remove 0 from main to play
+
+
+
+int main0603c()
+{
+    srand(15);
+
+    rand() % (Πλήθος τιμών Χmax + Xmin + 1) + Xmin
+
+
+        return 0;
+}
+
+
