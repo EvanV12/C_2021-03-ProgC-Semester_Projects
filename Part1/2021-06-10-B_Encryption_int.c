cb3c9bc59f96d4f08d0bf0aa6a249a53e5b3dad9
Evangelos V <evangv12@gmail.com>
2021-07-01 01:07:07 +0300

    restructuring

---
+++ Part1/2021-06-10-B_Encryption_int.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+//me don't know if correct
+int main()
+{
+
+    char t[300];
+    char k[20];
+    int i, j, length, error;
+
+    printf("Enter text: ");
+    fgets(t, 300, stdin);
+
+    do {
+        printf("Give Encryption key: ");
+        fgets(k, 20, stdin);
+
+        for (i = 0; k[i] != '\0'; i++) {
+            length += i;
+        }
+        error = (length < 4) ? 1 : 0;
+        if (error) printf("\nKey must be over 4 characters\n");
+    } while (error);
+
+
+    for (i = 0; t[i] != '\0'; i++) {
+        for (j = 0; k[j] != '\0'; j++) {
+            t[i] += k[j];
+        }
+    }
+    printf("Encrypted text is :%s\n", t);
+
+
+    printf("Give Decryption key: ");
+    fgets(k, 20, stdin);
+
+    for (i = 0; t[i] != '\0'; i++) {
+        for (j = 0; k[j] != '\0'; j++) {
+            t[i] -= k[j];
+        }
+    }
+
+    printf("Decrypted text is :%s\n", t);
+
+    return 0;
+
+}
+
